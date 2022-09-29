cdbe1030bb5c3dcc2d6d6be5bd1ba7869b48766c
R-Charles <Rapcharlo@aol.com>
2022-09-29 15:17:44 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dojosurverymodel1.Models;
+
+namespace dojosurverymodel1.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("")]
+
+    public ViewResult Index()
+    {
+        return View("Index");
+    }
+
+[HttpPost("/result")]
+
+    public IActionResult Submit(Result result)
+    {
+        if(ModelState.IsValid){
+            return View("result", result);
+        }else{
+            return View("Index");
+        }
+        
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dojosurverymodel1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
