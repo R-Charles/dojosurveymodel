cdfe5b5fb36ffecf0e3a096bcdbd5fb1e9a860a5
R-Charles <Rapcharlo@aol.com>
2022-09-29 17:40:01 -0700

    update

--- Models/Result.model.cs
+++ Models/Result.model.cs
@@ -1,10 +1,19 @@
 #pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
 
 public class Result
 {
+    [Required]
+    [MinLength(3)]
     public string Name { get; set; }
+    [Required]
+    [MinLength(3)]
     public string Location { get; set; }
+    [Required]
+    [MinLength(3)]
     public string Language { get; set; }
+    
+    [MinLength(25)]
     public string? Comment { get; set; }
 
 }
